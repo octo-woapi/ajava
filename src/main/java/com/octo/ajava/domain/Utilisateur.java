c427b65c0c4ea99defcd53972c52507dcfae9e08
Kévin JULIEN <kevin.julien@octo.com>
2022-09-30 10:22:38 +0200

    feat(testcontainers):  Mise en place de tests d'intégration avec une bdd postgresql

--- src/main/java/com/octo/ajava/domain/Utilisateur.java
+++ src/main/java/com/octo/ajava/domain/Utilisateur.java
@@ -1,17 +1,21 @@
 package com.octo.ajava.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import java.util.UUID;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "utilisateur")
 public class Utilisateur {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "nom")
     private String nom;
+
+    @Column(name = "prenom")
     private String prénom;
+
+    @Column(name = "password")
     private String motDePasse;
 }
