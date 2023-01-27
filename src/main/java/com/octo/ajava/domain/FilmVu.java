321aefe1037569e76a33021daf1e8594e95b5160
Christophe Charles <christophe.charles@octo.com>
2023-01-27 20:21:13 +0100

    chore: passage à Spring Boot 3

--- src/main/java/com/octo/ajava/domain/FilmVu.java
+++ src/main/java/com/octo/ajava/domain/FilmVu.java
@@ -1,11 +1,11 @@
 package com.octo.ajava.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.Objects;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 @Table(name = "film_vu")
