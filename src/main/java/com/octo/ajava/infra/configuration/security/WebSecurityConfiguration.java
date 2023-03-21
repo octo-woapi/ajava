2ca80bfbc86e790da7d24c0f109ea8fe32638333
Christophe Charles <christophe.charles@octo.com>
2023-03-21 13:49:40 +0100

    refactor: suppression des classes inutiles

--- src/main/java/com/octo/ajava/infra/configuration/security/WebSecurityConfiguration.java
+++ src/main/java/com/octo/ajava/infra/configuration/security/WebSecurityConfiguration.java
@@ -2,42 +2,39 @@ package com.octo.ajava.infra.configuration.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfiguration {
 
+  private static final RequestMatcher API_REQUESTS_MATCHER =
+      AntPathRequestMatcher.antMatcher("/api/**");
+  private static final RequestMatcher SWAGGER_REQUESTS_MATCHER =
+      AntPathRequestMatcher.antMatcher("/swagger-ui/**");
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    return http.headers()
-        .and()
-        .authorizeRequests()
-        .antMatchers("/api/**")
-        .permitAll()
-        .and()
-        .csrf()
-        .disable()
+    return http.securityMatcher(
+            request ->
+                API_REQUESTS_MATCHER.matches(request) || SWAGGER_REQUESTS_MATCHER.matches(request))
+        .headers(Customizer.withDefaults())
+        .sessionManagement(WebSecurityConfiguration::statelessSessionManagement)
+        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+        .csrf(AbstractHttpConfigurer::disable)
         .build();
   }
 
-  @Bean
-  public InMemoryUserDetailsManager userDetailsService() {
-    return new InMemoryUserDetailsManager(
-        User.withUsername("user")
-            .password(passwordEncoder().encode("password"))
-            .roles("USER")
-            .build());
-  }
-
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder(8);
+  private static void statelessSessionManagement(
+      SessionManagementConfigurer<HttpSecurity> session) {
+    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
   }
 }
