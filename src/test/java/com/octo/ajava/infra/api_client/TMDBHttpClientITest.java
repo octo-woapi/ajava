fd439e4ab5bc439689ddb8aa5b5c79ee9130fb3b
Christophe Charles <christophe.charles@octo.com>
2023-04-12 09:42:25 +0200

    test: ajout des tests repository et api client

--- src/test/java/com/octo/ajava/infra/api_client/TMDBHttpClientITest.java
+++ src/test/java/com/octo/ajava/infra/api_client/TMDBHttpClientITest.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TMDBHttpClientTest {
+class TMDBHttpClientITest {
 
   @Autowired private TMDBHttpClient tmdbHttpClient;
 
---
+++ src/test/java/com/octo/ajava/infra/repositories/TMDBFilmRepositoryUTest.java
@@ -0,0 +1,42 @@
+package com.octo.ajava.infra.repositories;
+
+import static com.octo.ajava.fixture.FilmTestBuilder.unFilm;
+import static com.octo.ajava.fixture.TMDBMovieTestBuilder.unTMDBMovie;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+import com.octo.ajava.domain.Film;
+import com.octo.ajava.infra.api_client.TMDBHttpClient;
+import com.octo.ajava.infra.api_client.entities.TMDBMovie;
+import com.octo.ajava.infra.mapper.TMDBFilmMapper;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class TMDBFilmRepositoryUTest {
+
+  @InjectMocks TMDBFilmRepository tmdbFilmRepository;
+  @Mock private TMDBHttpClient tmdbHttpClient;
+  @Mock private TMDBFilmMapper tmdbFilmMapper;
+
+  @Test
+  void recupererLesFilms_devrait_renvoyer_une_liste_de_films_TMDB() throws Exception {
+    // Given
+    TMDBMovie tmdbMovie = unTMDBMovie().build();
+    List<TMDBMovie> listDeFilmsTMDB = List.of(tmdbMovie);
+    List<Film> listDeFilmsAttendus = List.of(unFilm().avecTMDBMovie(tmdbMovie).build());
+    given(tmdbHttpClient.recupererLesFilmsPopulaires()).willReturn(listDeFilmsTMDB);
+    given(tmdbFilmMapper.convertirEnFilms(listDeFilmsTMDB)).willReturn(listDeFilmsAttendus);
+
+    // When
+    List<Film> filmsRecuperes = tmdbFilmRepository.recupererLesFilms();
+
+    // Then
+    assertThat(filmsRecuperes).isNotEmpty();
+    assertThat(filmsRecuperes).isEqualTo(listDeFilmsAttendus);
+  }
+}
